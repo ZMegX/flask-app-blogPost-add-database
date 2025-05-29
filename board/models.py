b65dd8f36b43b5b2c02aaa047f41b548aa954135
ZMegX <picardomegan@gmail.com>
2025-05-29 02:28:27 +0100

    models for post created - test connection postgres on render

--- board/models.py
+++ board/models.py
@@ -5,26 +5,31 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    __tablename__ = 'users'
+# will add user later and create the relationship
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+# class User(db.Model):
+#     __tablename__ = 'users'
 
-    # Relationship: One user can have many recipes
-    recipes = db.relationship('post', backref='author', lazy=True)
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+
+#     # Relationship: One user can have many recipes
+#     recipes = db.relationship('post', backref='author', lazy=True)
 
 class Post(db.Model):
     __tablename__ = 'post'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    author = db.Column(db.String, nullable=False)
+    message = db.Column(db.String, nullable=False)
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    def __repr__(self):
+        return f"<Post {self.id} by {self.author}>"
 
-    # Foreign key: link to user who posted it
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    # foreign key: link to user who posted it
+    #user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    # One recipe can have many ingredients
-    ingredients = db.relationship('Ingredient', backref='recipe', lazy=True)
+    # one recipe can have many ingredients
+    # ingredients = db.relationship('Ingredient', backref='recipe', lazy=True)
