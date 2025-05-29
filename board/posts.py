bba909714570cec938d9d7c91f36158843a8373f
ZMegX <picardomegan@gmail.com>
2025-05-29 03:55:55 +0100

    db.session instead of get_db calls

--- board/posts.py
+++ board/posts.py
@@ -1,30 +1,23 @@
 from flask import Blueprint, render_template, redirect, request, url_for
-
-from board.database import get_db
+from board.models import Post, db  # import your model and db session
 
 bp = Blueprint("posts", __name__)
 
-@bp.route("/create", methods=("GET", "POST"))
+@bp.route("/create", methods=["GET", "POST"])
 def create():
     if request.method == "POST":
-        author = request.form["author"] or "Anonymous"
-        message = request.form["message"]
+        author = request.form.get("author") or "Anonymous"
+        message = request.form.get("message")
 
         if message:
-            db = get_db()
-            db.execute(
-                "INSERT INTO post (author, message) VALUES (?, ?)",
-                (author, message),
-            )
-            db.commit()
+            new_post = Post(author=author, message=message)
+            db.session.add(new_post)
+            db.session.commit()
             return redirect(url_for("posts.posts"))
 
     return render_template("posts/create.html")
 
 @bp.route("/posts")
 def posts():
-    db = get_db()
-    posts = db.execute(
-        "SELECT author, message, created FROM post ORDER BY created DESC"
-    ).fetchall()    
+    posts = Post.query.order_by(Post.created.desc()).all()
     return render_template("posts/posts.html", posts=posts)
