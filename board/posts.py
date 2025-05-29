067d8f2bb56777f390c1cf169b5cffdd373dab9c
ZMegX <picardomegan@gmail.com>
2025-05-29 03:27:01 +0100

    rollabck

--- board/posts.py
+++ board/posts.py
@@ -1,23 +1,30 @@
 from flask import Blueprint, render_template, redirect, request, url_for
-from .models import Post, db  # import your model and db
+
+from board.database import get_db
 
 bp = Blueprint("posts", __name__)
 
-@bp.route("/create", methods=["GET", "POST"])
+@bp.route("/create", methods=("GET", "POST"))
 def create():
     if request.method == "POST":
-        author = request.form.get("author") or "Anonymous"
-        message = request.form.get("message")
+        author = request.form["author"] or "Anonymous"
+        message = request.form["message"]
 
         if message:
-            new_post = Post(author=author, message=message)
-            db.session.add(new_post)
-            db.session.commit()
+            db = get_db()
+            db.execute(
+                "INSERT INTO post (author, message) VALUES (?, ?)",
+                (author, message),
+            )
+            db.commit()
             return redirect(url_for("posts.posts"))
 
     return render_template("posts/create.html")
 
 @bp.route("/posts")
 def posts():
-    posts = Post.query.order_by(Post.created.desc()).all()
+    db = get_db()
+    posts = db.execute(
+        "SELECT author, message, created FROM post ORDER BY created DESC"
+    ).fetchall()    
     return render_template("posts/posts.html", posts=posts)
