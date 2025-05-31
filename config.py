78d6e89ce9a24d0b9b71e7db28694882ad0bdf4d
ZMegX <picardomegan@gmail.com>
2025-05-31 17:21:46 +0100

    test envirnment switch

--- config.py
+++ config.py
@@ -2,13 +2,15 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://testserver_pbjv_user:4y1kfym9fAj9I4GE9GTFgmY1VP0TEnes@dpg-d0rna6ili9vc73av1akg-a/testserver_pbjv')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
     DEBUG = True
 
 class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://testserver_pbjv_user:4y1kfym9fAj9I4GE9GTFgmY1VP0TEnes@dpg-d0rna6ili9vc73av1akg-a/testserver_pbjv')    
     DEBUG = False
 
 config_by_name = {
