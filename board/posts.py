6aeff1745c4911c01b72f13d2d45b3ddd935f6b3
ZMegX <picardomegan@gmail.com>
2025-06-04 16:57:57 +0100

    author is now not valid anymore - replace with username

--- board/posts.py
+++ board/posts.py
@@ -1,16 +1,23 @@
 from flask import Blueprint, render_template, redirect, request, url_for
-from board.models import Post, db  # import your model and db session
+from board.models import Post, db, User  # import your model and db session
 
 bp = Blueprint("posts", __name__)
 
 @bp.route("/create", methods=["GET", "POST"])
 def create():
     if request.method == "POST":
-        author = request.form.get("author") or "Anonymous"
+        username = request.form.get("author") or "Anonymous"
         message = request.form.get("message")
 
         if message:
-            new_post = Post(author=author, message=message)
+            user = User.query.filter_by(username=username).first()
+
+            if not user:
+                user = User(username=username)
+                db.session.add(user)
+                db.session.commit()
+
+            new_post = Post(author=user, message=message)
             db.session.add(new_post)
             db.session.commit()
             return redirect(url_for("posts.posts"))
