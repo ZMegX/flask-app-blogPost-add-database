928c3939e06c7a4d8182bebd849ac9b9a6a8f97c
ZMegX <picardomegan@gmail.com>
2025-06-04 19:24:15 +0100

    database psql updated

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///test3.db')
     DEBUG = True
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://testserver_pbjv_user:4y1kfym9fAj9I4GE9GTFgmY1VP0TEnes@dpg-d0rna6ili9vc73av1akg-a/testserver_pbjv')    
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://foodissimotest_user:61JGFa44MX6pioBgxbEiTUH7rr4dEOfT@dpg-d108af3e5dus739e0h8g-a/foodissimotest')    
     DEBUG = False
 
 config_by_name = {
