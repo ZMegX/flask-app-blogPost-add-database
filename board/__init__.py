3b86f19cc049e5367b4e7e760981fc71615a0cec
ZMegX <picardomegan@gmail.com>
2025-06-06 17:48:33 +0100

    bp errors_handler added

--- board/__init__.py
+++ board/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from dotenv import load_dotenv
 from config import config_by_name
 from .models import db 
-from . import pages, posts  # import your blueprints
+from . import pages, posts, errors  # import your blueprints
 
 
 load_dotenv()
@@ -25,7 +25,9 @@ def create_app():
     # Register blueprints
     app.register_blueprint(pages.bp)
     app.register_blueprint(posts.bp)
-
+    app.register_error_handler(404, errors.handle_404)
+    app.register_error_handler(500, errors.handle_500)
+    
     # Debug output
     print(f"Running in environment: {env}")
     print(f"DB URL: {app.config.get('SQLALCHEMY_DATABASE_URI')}")
