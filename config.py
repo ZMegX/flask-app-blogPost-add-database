7149a89385116d605e08d551a7b3e42b61e15767
ZMegX <picardomegan@gmail.com>
2025-06-07 23:14:44 +0100

    final commit

--- config.py
+++ config.py
@@ -9,6 +9,9 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///test3.db')
     DEBUG = True
 
+# when deploying on render you need to pass the internal URL of the database Server hosted on render.com
+# set up your environnement in the web app that you're deploying, all URL must match for prod
+
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://foodissimotest_user:61JGFa44MX6pioBgxbEiTUH7rr4dEOfT@dpg-d108af3e5dus739e0h8g-a/foodissimotest')    
     DEBUG = False
