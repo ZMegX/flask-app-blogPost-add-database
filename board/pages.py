0d8a3bc2bbe150a08af4b5224b6c148fb0cb105a
ZMegX <picardomegan@gmail.com>
2025-06-07 01:52:36 +0100

    bp to contact added

--- board/pages.py
+++ board/pages.py
@@ -9,3 +9,7 @@ def home():
 @bp.route("/about")
 def about():
     return render_template("pages/about.html")
+
+@bp.route("/contact")
+def contact():
+    return render_template("pages/contact.html")
